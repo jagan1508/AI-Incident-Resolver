69ba43e24b59aff3cb25dbc9004ebde94cd9b86f
jagan1508 <jaganathbabu025@gmail.com>
2026-06-30 12:22:16 +0530

    added redis layer

--- consumer.py
+++ consumer.py
@@ -1,9 +1,15 @@
 from kafka import KafkaConsumer
 import psycopg2
 import json
+import redis
 
 consumer = KafkaConsumer('demo', bootstrap_servers='localhost:9092', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-event_id=None
+
+
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
 try:
     with psycopg2.connect(
         host="localhost",
@@ -12,13 +18,17 @@ try:
             for message in consumer:
                 event = message.value
                 print(f"Received event: {event}")
-                cursor.execute(
-                    "INSERT INTO incidents (event_type, resource_name, fingerprint, raw_event, created_at) VALUES (%s, %s, %s, %s, %s) RETURNING id",
-                    (event["type"], event["resource"], event["fingerprint"], json.dumps(event), event["timestamp"]) 
-                )
-                row=cursor.fetchone()
-                if row:
-                    event_id=row[0]
-                conn.commit()
+                status=r.set(event["fingerprint"],json.dumps(event),ex=30,nx=True)
+                if status:          
+                    cursor.execute(
+                        "INSERT INTO incidents (event_type, resource_name, fingerprint, raw_event, created_at) VALUES (%s, %s, %s, %s, %s) RETURNING id",
+                        (event["type"], event["resource"], event["fingerprint"], json.dumps(event), event["timestamp"]) 
+                    )
+                    conn.commit()
+                    row=cursor.fetchone()
+                    if row:
+                        event_id=row[0]
+                else:
+                    print(f"Duplicate suppressed: {event['fingerprint']}")
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
